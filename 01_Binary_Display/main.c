f1435ec78b9a3b85674aefdb7ce90aed658859d7
jonaspeters85 <jonaspeters8526@gmail.com>
2025-12-31 16:33:06 +0100

    01 shows 3 Simple Binary Displays

--- 01_Binary_Display/main.c
+++ 01_Binary_Display/main.c
@@ -4,17 +4,22 @@
 #include <unistd.h>
 #include "../lib/binary_display.h"
 
-
-
-
+// Simple main function to demonstrate binary display normal, with colors and highlighted bit
 int main()
 {
     unsigned int number;
-    printf("Input number to display in binary: ");
+    printf("Input number to display in binary: \t");
     scanf("%u", &number);
-    printf("Binary representation of %u is: ", number);
 
+    printf("Binary representation of %u is: \t", number);
+    print_binary(number);
+
+    printf("Binary representation of %u is: \t", number);
     print_binary_color(number);
+
+    printf("Highlighting bit 3: \t\t\t");
+    print_binary_highlight(number, 3);
+
 
     return 0;
 }
