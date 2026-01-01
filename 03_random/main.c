5c1b1b4ffabd7eb4e81e31b0b0a03dea69d81d9c
jonaspeters85 <jonaspeters8526@gmail.com>
2026-01-01 17:04:33 +0100

    3 examples with upper and lower case

--- 03_random/main.c
+++ 03_random/main.c
@@ -8,31 +8,49 @@
 
 #define NL printf("\n");
 
+int check_ifalpha(unsigned int c)
+{
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
 void to_lowercaser(char *s)
 {
     while(*s != '\0')
-        *s++ = *s | (1u << 5);
+    {
+        if (check_ifalpha(*s))
+            *s = *s | (1u << 5);
+        s++;
+    }
 }
 
 void to_uppercaser(char *s)
 {
     while(*s != '\0')
-        *s++ = *s & ~(1u << 5);
+    {
+        if (check_ifalpha(*s))
+            *s = *s & ~(1u << 5);
+        s++;
+    }
 }
 
 void switch_lower_upper_caser(char *s)
 {
     while(*s != '\0')
-        *s++ = *s ^ (1u << 5);
+    {
+        if (check_ifalpha(*s))
+            *s = *s ^ (1u << 5);
+        s++;
+    }
 }
 
-
-// random tests
-int main()
+void test_bit_set()
 {
-    printf("Using bitshifting to set a Captial letter to lowercase letter\n");
+    printf("----------------------------------------------------------------\n");
+    printf(COLOR_GREEN "Using bitshifting and bit mask to set a Captial letter to lowercase letter\n" COLOR_RESET);
     unsigned int ASCII_A = 65;
     unsigned int ASCII_a = 97;
+
+    NL
 
     printf("Binary representation of ASCII 'A' (%u) is: \t", ASCII_A);
     print_binary_high_is_green(ASCII_A);
@@ -40,9 +58,11 @@ int main()
     print_binary_high_is_green(ASCII_a);
 
     NL
+    printf("the difference between 'A' and 'a' is the 6th bit (bit 5, starting at 0)\n");
+    NL
 
-    printf("check 6th bit (5) of 'A' %u \n", ASCII_A & (1u << 5));
-    printf("check 6th bit (5) of 'a' %u \n", ASCII_a & (1u << 5));
+    printf("check 6th bit (5) of 'A' with  ASCII_A & (1u << 5) = %u \n", ASCII_A & (1u << 5));
+    printf("check 6th bit (5) of 'a' with  ASCII_a & (1u << 5) = %u \n", ASCII_a & (1u << 5));
 
     NL
 
@@ -51,11 +71,9 @@ int main()
     printf("hightlight 6th bit of 'a': ");
     print_binary_highlight(ASCII_a, 5);
 
-
-
     NL
 
-    printf("set 6th bit (5) high\nASCII_A = ASCII_A | (1u << 5);\n");
+    printf("set 6th bit (5) high\nASCII_A = "COLOR_PURPLE"ASCII_A | (1u << 5);\n" COLOR_RESET);
     ASCII_A = ASCII_A | (1u << 5);
 
     printf("hightlight 6th bit of 'A': ");
@@ -97,6 +115,79 @@ int main()
     printf("Test String: %s\n", test_str); 
     to_lowercaser(test_str);
     printf("\nresult: %s\n", test_str);
+
+    NL
+    NL
+}
+
+void test_clear_bit()
+{
+    printf("----------------------------------------------------------------\n");
+
+    printf(COLOR_RED "Using bitshifting and bit mask to clear a lowercase letter to Capital letter\n" COLOR_RESET);
+    unsigned int ASCII_A = 65;
+    unsigned int ASCII_a = 97;
+
+    NL
+
+    printf("Binary representation of ASCII 'A' (%u) is: \t", ASCII_A);
+    print_binary_high_is_green(ASCII_A);
+    printf("Binary representation of ASCII 'a' (%u) is: \t", ASCII_a);
+    print_binary_high_is_green(ASCII_a);
+
+    NL
+    printf("the difference between 'A' and 'a' is the 6th bit (bit 5, starting at 0)\n");
+    NL
+
+    printf("check 6th bit (5) of 'A' with  ASCII_A & (1u << 5) = %u \n", ASCII_A & (1u << 5));
+    printf("check 6th bit (5) of 'a' with  ASCII_a & (1u << 5) = %u \n", ASCII_a & (1u << 5));
+
+    NL
+
+    printf("hightlight 6th bit of 'A': ");
+    print_binary_highlight(ASCII_A, 5);
+    printf("hightlight 6th bit of 'a': ");
+    print_binary_highlight(ASCII_a, 5);
+
+    NL
+
+    printf("clear 6th bit (5) low\nASCII_a = "COLOR_PURPLE"ASCII_a & ~(1u << 5);\n" COLOR_RESET);
+    ASCII_a = ASCII_a & ~(1u << 5);
+
+    printf("hightlight 6th bit of 'a': ");
+    print_binary_highlight(ASCII_a, 5);
+
+    NL
+
+    char test_str[] ="Hello, World!";
+
+    printf("Test String: %s\n", test_str); 
+    to_uppercaser(test_str);
+    printf("\nresult: %s\n", test_str);
+}
+
+void test_switch_case()
+{
+    printf("----------------------------------------------------------------\n");
+    printf(COLOR_YELLOW "Using bitshifting and bit mask to switch case of letters\n" COLOR_RESET);
+    
+    NL
+    printf("Using XOR (^) operator to toggle the 6th bit (5) to switch case\n");
+    printf("number = number ^ (1u << 5)");
+    
+    char test_str[] ="Hello, World!";
+
+    printf("Test String: %s\n", test_str); 
+    switch_lower_upper_caser(test_str);
+    printf("\nresult: %s\n", test_str);
+}
+
+// random tests
+int main()
+{
+    test_bit_set();
+    test_clear_bit();
+    test_switch_case();
     return 0;
 }
 
