5cd601eea807b44d0a52382683a6810e5a821dec
jonaspeters85 <jonaspeters8526@gmail.com>
2026-01-04 00:55:40 +0100

    start bitshifting

---
+++ 04_Bit_Shifting/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include "../lib/binary_display.h"
+#include "../lib/colors.h"
+
+#define NL printf("\n");
+
+// Bit Shifting
+int main()
+{
+    printf("Bit shifting\n============\n");
+
+
+    return 0;
+}
+
