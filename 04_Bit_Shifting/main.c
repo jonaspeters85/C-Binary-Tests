65676eea8df7999dae3c66ed6074bac0159c60d9
jonaspeters85 <jonaspeters8526@gmail.com>
2026-01-04 16:00:53 +0100

    04 bit shifting and or operator

--- 04_Bit_Shifting/main.c
+++ 04_Bit_Shifting/main.c
@@ -14,6 +14,14 @@ int main()
 {
     printf("Bit shifting\n============\n");
 
+    int n = 7;
+    //printf("n = %u   (n << 1) = %u \n", n, (n << 1));
+
+    printf("Builing a 24 bit Value with 3 8bit Values i.e. for RGB Colores\n");
+    unsigned int res = (254u << 16) | (1u << 8) | (3 << 0);
+    printf("res = (254u << 16) | (1u << 8) | (2 << 0).  = %u\n", res);
+
+    print_binary_high_is_green(res);
 
     return 0;
 }
